6abbf203fabf875448bd50f768557136ad3c5219
gurigura <>
2019-08-16 10:35:41 +0900

    readme修正

--- README.md
+++ README.md
@@ -1,6 +1,106 @@
 # myVue.jsGoogleMap
 https://gurigura0052.github.io/myVue.jsGoogleMap
 
+## 概要
+
+## プラグインインストール
+### axios
+```
+$ npm install --save axios
+```
+https://www.npmjs.com/package/axios
+
+### google-maps
+```
+$ npm install --save google-maps
+```
+https://www.npmjs.com/package/google-maps
+
+### jQuey
+```
+$ npm install --save jquery
+```
+
+## Mapコンポーネント
+src/components//Map.vue
+```html
+<div v-else id="gglMap"></div>
+```
+
+```javascript
+request: {
+  url: '/json/map_marker.json', // マーカーデータの場所
+  mapData: {
+    ID: 'gglMap', // mapを表示させる場所
+    center: {
+      // マップのデフォルト中心座標
+      lat: 35.6954806,
+      lng: 139.76325010000005
+    },
+    zoom: 15, // マップの倍率
+    scrollWheel: false // ホイールスクロールを無効にする
+  }
+},
+```
+グーグルマップで必要な設定をrequest内に入れておきます。
+マーカーデータのjsonはpublic以下に置きます。
+ホイールスクロールはデフォルトで有効になっているので、無効にするために設定します。
+
+```javascript
+const response = await axios.get(request.url)
+  .catch(() => status = 'error') // 失敗した時の処理
+```
+axiosでjsonデータを取ってきます。
+
+```javascript
+await GoogleMapsLoader.load(function(google) {
+  const map = new google.maps.Map(
+    document.getElementById(request.mapData.ID),
+    {
+      zoom: request.mapData.zoom,
+      center: request.mapData.center,
+      scrollwheel: request.mapData.scrollWheel
+    }
+  )
+```
+プラグイン「google-maps」を使い、マップの初期設定をします。
+
+```javascript
+for (let i in markerData) {
+  LatLng[i] = new google.maps.LatLng(
+    markerData[i]['lat'],
+    markerData[i]['lng']
+  ) // 各マーカーの座標を取得
+
+  marker[i] = new google.maps.Marker({
+    position: LatLng[i],
+    map: map
+  })
+
+  // 情報ウィンドウ
+  contentString[i] = `<div>${markerData[i]['text']}</div>` // 情報ウィンドウ内のhtml
+
+  infowindow[i] = new google.maps.InfoWindow({
+    content: contentString[i]
+  })
+
+  markerEvent(i)
+}
+```
+マーカー毎の設定をします。
+
+```javascript
+function markerEvent(i) {
+  marker[i].addListener('click', function() {
+    $(infowindow).each(function(i){
+      infowindow[i].close() // マーカーをクリックする度に全部のウィンドウを閉じる
+    })
+    infowindow[i].open(map, marker[i]) // クリックしたマーカーのウィンドウを開く
+  })
+}
+```
+マーカークリック時の挙動を設定します。
+
 ## Project setup
 ```
 npm install
@@ -29,20 +129,4 @@ npm run lint
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
 
-## プラグインインストール
-### axios
-```
-$ npm install --save axios
-```
-https://www.npmjs.com/package/axios
 
-### google-maps
-```
-$ npm install --save google-maps
-```
-https://www.npmjs.com/package/google-maps
-
-### jQuey
-```
-$ npm install --save jquery
-```
--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,18 +1,21 @@
 <template>
-  <div id="gglMap"></div>
+  <div>
+    <div v-if="status == 'error'">{{status}}</div>
+    <div v-else id="gglMap"></div>
+  </div>
 </template>
 
 <script>
-import GoogleMapsLoader from "google-maps";
+import GoogleMapsLoader from 'google-maps'
 
 export default {
-  name: "gglMap",
+  name: 'gglMap',
   data() {
     return {
       request: {
-        url: "./json/map_marker.json", // マーカーデータの場所
+        url: '/json/map_marker.json', // マーカーデータの場所
         mapData: {
-          ID: "gglMap", // mapを表示させる場所
+          ID: 'gglMap', // mapを表示させる場所
           center: {
             // マップのデフォルト中心座標
             lat: 35.6954806,
@@ -22,79 +25,79 @@ export default {
           scrollWheel: false // ホイールスクロールを無効にする
         }
       },
-      status: ""
-    };
+      status: ''
+    }
   },
 
   created: function() {
-    this.gglMap();
+    this.gglMap()
   },
 
   methods: {
-    gglMap: function() {
-      var _this = this;
+    async gglMap() {
+      const request = this.request
+      let status = this.status
 
-      axios
-        .get(_this.request.url)
-        .then(function(response) {
-          _this.status = "success";
+      const response = await axios.get(request.url)
+        .catch(() => status = 'error') // 失敗した時の処理
+        
+      if( status != 'error' ){ // 成功した時の処理
+        status = 'success'
 
-          GoogleMapsLoader.load(function(google) {
-            var map = new google.maps.Map(
-              document.getElementById(_this.request.mapData.ID),
-              {
-                zoom: _this.request.mapData.zoom,
-                center: _this.request.mapData.center,
-                scrollwheel: _this.request.mapData.scrollWheel
-              }
-            );
-
-            var markerData = response.data.marker; // マーカーデータをmarkerDataに入れる
-
-            // マーカー毎の処理
-            var LatLng = [];
-            var marker = [];
-            var contentString = [];
-            var infowindow = [];
-
-            for (var i = 0; i < markerData.length; i++) {
-              LatLng[i] = new google.maps.LatLng(
-                markerData[i]["lat"],
-                markerData[i]["lng"]
-              ); // 各マーカーの座標を取得
-
-              marker[i] = new google.maps.Marker({
-                position: LatLng[i],
-                map: map
-              });
-
-              // 情報ウィンドウ
-              contentString[i] = "<div>" + markerData[i]["text"] + "</div>"; // 情報ウィンドウ内のhtml
-
-              infowindow[i] = new google.maps.InfoWindow({
-                content: contentString[i]
-              });
-
-              markerEvent(i);
+        await GoogleMapsLoader.load(function(google) {
+          const map = new google.maps.Map(
+            document.getElementById(request.mapData.ID),
+            {
+              zoom: request.mapData.zoom,
+              center: request.mapData.center,
+              scrollwheel: request.mapData.scrollWheel
             }
+          )
 
-            function markerEvent(i) {
-              marker[i].addListener("click", function() {
-                $(infowindow).each(function(i){
-                  infowindow[i].close(); // マーカーをクリックする度に全部のウィンドウを閉じる
-                })
-                infowindow[i].open(map, marker[i]); // クリックしたマーカーのウィンドウを開く
-              });
-            }
-          });
+          const markerData = response.data.marker // マーカーデータをmarkerDataに入れる
+
+          // マーカー毎の処理
+          let LatLng = []
+          let marker = []
+          let contentString = []
+          let infowindow = []
+
+          for (let i in markerData) {
+            LatLng[i] = new google.maps.LatLng(
+              markerData[i]['lat'],
+              markerData[i]['lng']
+            ) // 各マーカーの座標を取得
+
+            marker[i] = new google.maps.Marker({
+              position: LatLng[i],
+              map: map
+            })
+
+            // 情報ウィンドウ
+            contentString[i] = `<div>${markerData[i]['text']}</div>` // 情報ウィンドウ内のhtml
+
+            infowindow[i] = new google.maps.InfoWindow({
+              content: contentString[i]
+            })
+
+            markerEvent(i)
+          }
+
+          function markerEvent(i) {
+            marker[i].addListener('click', function() {
+              $(infowindow).each(function(i){
+                infowindow[i].close() // マーカーをクリックする度に全部のウィンドウを閉じる
+              })
+              infowindow[i].open(map, marker[i]) // クリックしたマーカーのウィンドウを開く
+            })
+          }
         })
+      }
 
-        .catch(function() {
-          _this.status = "error"; // map_marker.jsのデータを取り出せなかった時の処理
-        });
+      this.status = status
     }
   }
-};
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
